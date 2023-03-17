065eedd40423385ef6d60297d7b5d031d6025fce
joe-dipilato <joe.dipilato@joe.dipilato.mbp.home>
2023-03-16 21:38:02 -0400

    adding settings files

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
+
+# VSCODE
 BASEDIR=$(dirname "$0" | realpath)
 ln -s ${BASEDIR}/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+# Starship
+mkdir -p ~/.config
+ln -s ${BASEDIR}/starship.toml ~/.config/
